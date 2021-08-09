439b3b0d80265c7f3792747aacca7873a5267055
Abhimanyu Kumar <86499459+abhirock07@users.noreply.github.com>
2021-08-09 17:12:15 -0400

    Added Even binding concept
    <button [disabled]="!allowNewServer" (click)="onCreateServer()">Add Server</button> --> Property Binding concept
    <!--Even Bininding Syntax is () -->

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
 
+  serverCreationStatus = 'No Server was Created';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;// ES 6 errow function syntax: ()= >
@@ -19,4 +21,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server Was Created';
+  }
 }
