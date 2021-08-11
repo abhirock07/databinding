b809ad82ccf7300ad8e7316e02b5cf70a5db38dc
Abhimanyu Kumar <86499459+abhirock07@users.noreply.github.com>
2021-08-11 16:44:11 -0400

    Added FormsModule for two way binding
    To used two way binding [()], need to import FormsModule in the app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -10,11 +10,14 @@ import { ServersComponent } from './servers/servers.component';
   declarations: [
     AppComponent,
     ServersComponent,
-    ServerComponent
+    ServerComponent,
+  
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
